f13a85f4ea3af3735db844029b421dff3341bbe2
nrmnkny <normane7@icloud.com>
2024-11-12 16:21:21 -0500

    API test-1

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const postRoutes = require('./routes/postRoutes');
 const authRoutes = require('./routes/authRoutes'); 
 const authMiddleware = require('./middleware/authMiddleware'); 
 
+app.get('/', (req, res) => res.send("Welcome to the Fleurs API"));
+
 app.use('/api/posts', postRoutes);
 
 app.use('/api', authRoutes);
 
 app.use(authMiddleware); 
 app.use('/api/admin/posts', postRoutes); 
-
-app.get('/', (req, res) => res.send("Welcome to the Fleurs API"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
